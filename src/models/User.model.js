44c3b1b913058b64478fc83f329eacb699ab76b9
szaratep <sebaszarate2005@gmail.com>
2026-05-24 23:46:16 -0500

    [+] User.model.js, se actualiza el modelo de usuario agregando nuevos campos al momento de crearlo

--- src/models/User.model.js
+++ src/models/User.model.js
@@ -1,11 +1,56 @@
 import { Schema, Types, model } from 'mongoose';
 
 //Primera parte: definir el schema
+const addressesSchema = new Schema({
+    alias: {
+        type: String,
+        required: true
+    },
+
+    street: {
+        type: String,
+        required: true
+    },
+
+    city: {
+        type: String,
+        required: true
+    },
+
+    state: {
+        type: String,
+        required: true
+    },
+
+    zipCode: {
+        type: String
+    },
+
+    country: {
+        type: String,
+        default: 'Colombia'
+    },
+
+    isDefault: {
+        type: Boolean,
+        default: false
+    }
+    
+
+}, {_id: false});
+
+
 const UserSchema = new Schema({
     name: {
         type: String,
         required: true,
         trim:true
+    },
+
+    lastName: {
+        type: String,
+        required: true,
+        trim: true
     },
     
     userName: {
@@ -28,12 +73,21 @@ const UserSchema = new Schema({
         trim: true
     },
 
+    role: {
+        type: String,
+        roleType: ["admin", "vendedor", "comprador"],
+        default: "comprador",
+        required: true
+    },
+
     phoneNumber: {
         type: String,
         minLength: 10,
         maxLength: 10,
         trim: true
     },
+
+    addresses: [addressesSchema],
 
     birthdate: {
         type: Date,
